b7ddd79e33f5033898aac81e3e73921f773596f3
idiomatic-go <113379405+idiomatic-go@users.noreply.github.com>
2022-09-21 15:29:14 -0500

    Startup entry

---
+++ accesslog/startup.go
@@ -0,0 +1,16 @@
+package accesslog
+
+import (
+	"github.com/idiomatic-go/entity-data/accesslog"
+)
+
+func Startup() {
+	view := accesslog.AccessLogView{}
+	if len(view.Headers) > 0 {
+
+	}
+	//log := v1.StreamAccessLogsMessage{}
+	//if log.ID != "" {
+	//
+	//}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/idiomatic-go/client-exchange
+
+go 1.18
+
+require github.com/idiomatic-go/entity-data v0.0.0-20220921201010-38952905d72d
